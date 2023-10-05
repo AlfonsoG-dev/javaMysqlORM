f6c8d621fc8e1e7b0a983a88dfc2e395171e5055
tuNovio <alfgomachi@gmail.com>
2023-10-05 14:43:38 -0500

    refactor: por el momento no es posible crear todas las funciones para migration

--- src/App.java
+++ src/App.java
@@ -1,6 +1,5 @@
 import Conexion.*;
 import Mundo.User;
-import Utils.*;
 
 public class App {
     public static void main(String[] args) {
@@ -8,8 +7,6 @@ public class App {
             UserDAO miUserDAO = new UserDAO();
             System.out.println("usuarios: " + miUserDAO.ReadAll().length);
             User nuevo = new User(0, "juan", "jl@gmail", "123", "user", null, null);
-            MigrationBuilder migrate = new MigrationBuilder("users");
-            migrate.CreateTableQuery("users", nuevo);
             nuevo.setCreate_at();
             //miUserDAO.InsertNewRegister(nuevo);
             //miUserDAO.UpdateRegister(nuevo, "nombre: juan, password: 123");
