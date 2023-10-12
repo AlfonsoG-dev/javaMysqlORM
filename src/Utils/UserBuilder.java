6bae6559c69ab5bf8a8252d413652c961ae27618
tuNovio <alfgomachi@gmail.com>
2023-10-11 23:26:30 -0500

    refactor: modificar la forma en la que se crea un usuario

--- src/Utils/UserBuilder.java
+++ src/Utils/UserBuilder.java
@@ -13,7 +13,7 @@ public record UserBuilder() {
      * valida los datos del ResultSet
      * @return lista de datos
      */
-    public String[] DataValidator(ResultSet rst, int capacity) throws SQLException {
+    private String[] ResultDataValidator(ResultSet rst, int capacity) throws SQLException {
         String[] data = new String[capacity + 1];
         for(int i = 1; i < data.length; i++) {
             data[i] = rst.getString(i);
@@ -25,11 +25,23 @@ public record UserBuilder() {
     
     /**
      * crea el usuario con los datos del ResultSet
+     * @param rst: el resultado de la consulta sql
+     * @param capacity: tamaño de la lista de datos
      * @return usuario creado con los datos
      */
-    public User CreateNewUser(ResultSet rst, int capacity) throws SQLException {
-        String[] data = this.DataValidator(rst, capacity);
+    public User CreateNewUserFromRST(ResultSet rst, int capacity) throws SQLException {
+        String[] data = this.ResultDataValidator(rst, capacity);
         User nUser = new User(Integer.parseInt(data[1]), data[2], data[3], data[4], data[5], data[6], data[7]);
+        return nUser;
+    }
+    /**
+     * crea el usuario con los datos que llegan por parametro en forma de String
+     * @param datos: datos del usuario a crear
+     * @return el usuario creado a partir de los daots
+     */
+    public User CreateNewUserFromSTR(String datos) {
+        String[] validate = datos.split(",");
+        User nUser = new User(Integer.parseInt(validate[0]), validate[1], validate[2], validate[3], validate[4], validate[5], validate[6]);
         return nUser;
     }
 }
