b9388f77d5208bfdb6d0b381bf4bc48cf09b18e0
tuNovio <alfgomachi@gmail.com>
2023-10-25 23:13:13 -0500

    refactor: modificar el compilador

--- java-exe.ps1
+++ java-exe.ps1
@@ -1,5 +1,5 @@
-$Clases = " ./src/*.java"
+$Clases = " ./src/*.java ./src/Conexion/*.java ./src/Conexion/Migration/*.java ./src/Conexion/Query/*.java ./src/Config/*.java ./src/Model/*.java ./src/Mundo/Users/*.java ./src/Mundo/Cuentas/*.java ./src/Utils/*.java"
 $Compilation = "javac -d ./bin/" + "$Clases";
 $javaCommand = "java -cp " + '"./bin;d:/Default/Proyectos/dependencias/mysql-connector-j-8.1.0/mysql-connector-j-8.1.0.jar" .\src\App.java';
 $runCommand = "$Compilation" + " & " + "$javaCommand"
-Invoke-Expression $javaCommand
+Invoke-Expression $runCommand
