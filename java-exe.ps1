3e3349833d74ec4fff8d3bddf9357f88478bf1f9
tuNovio <alfgomachi@gmail.com>
2023-10-27 13:33:44 -0500

    agregar jdbc a lib e ignorar lib para el repositorio

--- java-exe.ps1
+++ java-exe.ps1
@@ -1,5 +1,5 @@
 $Clases = " ./src/*.java ./src/Conexion/*.java ./src/Conexion/Migration/*.java ./src/Conexion/Query/*.java ./src/Config/*.java ./src/Model/*.java ./src/Mundo/*.java ./src/Mundo/Users/*.java ./src/Mundo/Cuentas/*.java ./src/Utils/*.java"
 $Compilation = "javac -d ./bin/" + "$Clases";
-$javaCommand = "java -cp " + '"./bin;d:/Default/Proyectos/dependencias/mysql-connector-j-8.1.0/mysql-connector-j-8.1.0.jar" .\src\App.java';
+$javaCommand = "java -cp " + '"./bin;./lib/mysql-connector-j-8.1.0/mysql-connector-j-8.1.0.jar" .\src\App.java';
 $runCommand = "$Compilation" + " & " + "$javaCommand"
 Invoke-Expression $runCommand
