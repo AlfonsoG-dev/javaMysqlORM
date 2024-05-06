ab565e6038d4e624be05979e185c0062bba4ea72
AlfonsoG-dev <alfgomachi@gmail.com>
2024-05-05 20:29:33 -0500

    ci: update build script

--- java-exe.ps1
+++ java-exe.ps1
@@ -1,4 +1,4 @@
-$compile = "javac -Xlint:all -d .\bin\ -cp '.\lib\mysql-connector-j-8.3.0\mysql-connector-j-8.3.0.jar' .\src\Conexion\*.java .\src\Conexion\Migration\*.java .\src\Conexion\Query\*.java .\src\Config\*.java .\src\Model\*.java .\src\Mundo\*.java .\src\Mundo\Cuentas\*.java .\src\Mundo\Users\*.java .\src\Samples\*.java .\src\Samples\Migration\*.java .\src\Utils\*.java "
+$compile = "javac -Werror -Xlint:all -d .\bin\ -cp '.\lib\mysql-connector-j-8.3.0\mysql-connector-j-8.3.0.jar' .\src\Conexion\*.java .\src\Conexion\Migration\*.java .\src\Conexion\Query\*.java .\src\Config\*.java .\src\Model\*.java .\src\Mundo\*.java .\src\Mundo\Cuentas\*.java .\src\Mundo\Samples\*.java .\src\Mundo\Samples\Migration\*.java .\src\Mundo\Samples\Models\*.java .\src\Mundo\Samples\Normal\*.java .\src\Mundo\Users\*.java .\src\Utils\Builder\*.java .\src\Utils\Model\*.java .\src\Utils\Query\*.java "
 $createJar = "jar -cfm javaMysqlORM.jar Manifesto.txt -C .\bin\ . -C .\extractionFiles\mysql-connector-j-8.3.0\ ."
 $runCommand = "$compile" + " && " + "$createJar"
 Invoke-Expression $runCommand 
