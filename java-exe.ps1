53eb1baf961facfe38463bcce26aa7b94e9fb374
AlfonsoG-dev <alfgomachi@gmail.com>
2024-05-19 00:57:12 -0500

    build: update build script

--- java-exe.ps1
+++ java-exe.ps1
@@ -1,4 +1,6 @@
-$compile = "javac -Werror -Xlint:all -d .\bin\ -cp '.\lib\mysql-connector-j-8.3.0\mysql-connector-j-8.3.0.jar' .\src\Conexion\*.java .\src\Conexion\Migration\*.java .\src\Conexion\Query\*.java .\src\Config\*.java .\src\Model\*.java .\src\Mundo\*.java .\src\Mundo\Cuentas\*.java .\src\Mundo\Samples\*.java .\src\Mundo\Samples\Migration\*.java .\src\Mundo\Samples\Models\*.java .\src\Mundo\Samples\Normal\*.java .\src\Mundo\Users\*.java .\src\Utils\Builder\*.java .\src\Utils\Model\*.java .\src\Utils\Query\*.java "
+$srcClases = ".\src\Conexion\*.java .\src\Conexion\Migration\*.java .\src\Conexion\Query\*.java .\src\Config\*.java .\src\Model\*.java .\src\Mundo\*.java .\src\Mundo\Cuentas\*.java .\src\Mundo\Samples\*.java .\src\Mundo\Samples\Migration\*.java .\src\Mundo\Samples\Models\*.java .\src\Mundo\Samples\Normal\*.java .\src\Mundo\Users\*.java .\src\Utils\Builder\*.java .\src\Utils\Formats\*.java .\src\Utils\Model\*.java .\src\Utils\Query\*.java "
+$libFiles = ".\lib\mysql-connector-j-8.3.0\mysql-connector-j-8.3.0.jar;"
+$compile = "javac -Werror -Xlint:all -d .\bin\ -cp '$libFiles' $srcClases"
 $createJar = "jar -cfm javaMysqlORM.jar Manifesto.txt -C .\bin\ . -C .\extractionFiles\mysql-connector-j-8.3.0\ ."
-$runCommand = "$compile" + " && " + "$createJar"
+$runCommand = "$compile" + " && " + "$createJar" 
 Invoke-Expression $runCommand 
